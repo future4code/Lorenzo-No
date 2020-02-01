17c6289cd8d9115e258a724faa6964d93a1c3e90
unknown <lorenzono47@gmail.com>
2020-01-31 23:20:53 -0300

    Semana 12 - Projeto

--- semana12/aula3-testes-redux-thunk/src/containers/Planner/index.js
+++ semana12/aula3-testes-redux-thunk/src/containers/Planner/index.js
@@ -3,7 +3,26 @@ import { connect } from "react-redux";
 
 class Planner extends React.Component {
   render() {
-    return <div />;
+    return <div>
+      <div>
+        <label htmlFor="task">Tarefa</label>
+        <input id="task" type="text" placeholder="Nova tarefa" />
+        <select id="weekday">
+          <option value="" disabled selected>Escolha um dia</option>
+          <option value="monday">Segunda</option>
+          <option value="tuesday">Terça</option>
+          <option value="wednesday">Quarta</option>
+          <option value="thursday">Quinta</option>
+          <option value="friday">Sexta</option>
+          <option value="saturday">Sábado</option>
+          <option value="sunday">Domingo</option>
+        </select>
+        <button>Criar</button>
+      </div>
+      <div>
+
+      </div>
+    </div>;
   }
 }
 
